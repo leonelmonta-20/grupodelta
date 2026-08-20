1b4e6bde0c304d047c91124000a43ed035d330f7
leonelmonta-20 <lmontanoangulo01@gmail.com>
2026-08-20 14:25:57 -0400

    init_0
    first

---
+++ ggdelta/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ggdelta
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola LEONEL ...TENGO QUE ESTUDIAR...deja de apostar con combos en metabet :=)!");
+        }
+    }
+}
